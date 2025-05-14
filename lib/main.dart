223dee514bbb98eb12a6c4da2915d6584680cb6b
Mahmut Basmacı <148640537+MahmutB3@users.noreply.github.com>
2025-05-15 01:06:42 +0300

    Edit the app name and icon

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Deepfake Kontrol',
+      title: 'Mask Off',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
