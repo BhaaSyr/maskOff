3b08deca852f1b9980b0a21a918801a5619154dd
Mahmut Basmacı <148640537+MahmutB3@users.noreply.github.com>
2025-06-02 00:35:24 +0300

    .

--- lib/generated/l10n.dart
+++ lib/generated/l10n.dart
@@ -18,10 +18,8 @@ class S {
   static S? _current;
 
   static S get current {
-    assert(
-      _current != null,
-      'No instance of S was loaded. Try to initialize the S delegate before accessing S.current.',
-    );
+    assert(_current != null,
+        'No instance of S was loaded. Try to initialize the S delegate before accessing S.current.');
     return _current!;
   }
 
@@ -43,10 +41,8 @@ class S {
 
   static S of(BuildContext context) {
     final instance = S.maybeOf(context);
-    assert(
-      instance != null,
-      'No instance of S present in the widget tree. Did you add S.delegate in localizationsDelegates?',
-    );
+    assert(instance != null,
+        'No instance of S present in the widget tree. Did you add S.delegate in localizationsDelegates?');
     return instance!;
   }
 
@@ -806,7 +802,12 @@ class S {
 
   /// `OK`
   String get ok {
-    return Intl.message('OK', name: 'ok', desc: 'OK button text', args: []);
+    return Intl.message(
+      'OK',
+      name: 'ok',
+      desc: 'OK button text',
+      args: [],
+    );
   }
 
   /// `An error occurred during password reset`
@@ -891,7 +892,12 @@ class S {
 
   /// `Age`
   String get age {
-    return Intl.message('Age', name: 'age', desc: 'Age field label', args: []);
+    return Intl.message(
+      'Age',
+      name: 'age',
+      desc: 'Age field label',
+      args: [],
+    );
   }
 
   /// `Save`
